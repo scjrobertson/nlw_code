9549657396bdd57db574cb53b22860e51bd7f7c2
scj_robertson <robertsonscj@gmail.com>
2015-08-19 13:17:51 +0200

    Bash script to run main

---
+++ runmain.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SNLG="src/main/resources/simplenlg-v4.4.2.jar";
+CPATH="src/main/trees/RunMain.java"
+COMPILE="src.main.trees.RunMain";
+
+input=$1;
+
+inpath="src/main/output/$input";
+parse="$inpath/$input""_parse.txt";
+dep="$inpath/$input""_dep.txt";
+lemma="$inpath/$input""_lemma.txt";
+
+echo $parse
+echo $dep
+echo $lemma
+javac -cp .:$SNLG $CPATH;
+java -cp .:$SNLG $COMPILE $parse $dep $lemma;
